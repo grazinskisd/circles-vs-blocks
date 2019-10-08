d141ff94e31d97f9b27f970d540ea5b2372b82a3
Dalius Grazinskis <grazinskisd@gmail.com>
2019-10-08 22:06:45 +0200

    Add basic upgrade button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,17 +9,62 @@ namespace CvB
     {
         public Enemy enemy;
         public ResourceView resourceView;
+        public UpgradeView upgradeView;
 
         private float _gold = 0;
         private int _level = 1;
+        private int _nextCost = 0;
 
         private void Start()
         {
+            _nextCost = GetUpgradeCost();
+            SetupEnemyListener();
+            SetupUpgradeView();
+        }
+
+        private void SetupUpgradeView()
+        {
+            CheckIfCanUpgrade();
+            upgradeView.ShowUpgradeCost(_nextCost);
+            upgradeView.OnClick += () =>
+            {
+                _gold -= _nextCost;
+                _level++;
+                _nextCost = GetUpgradeCost();
+                upgradeView.ShowUpgradeCost(_nextCost);
+                CheckIfCanUpgrade();
+                UpdateResourceView();
+            };
+        }
+
+        private void SetupEnemyListener()
+        {
             enemy.OnClicked += () =>
             {
-                _gold += 5 * Mathf.Pow(_level, 2.1F);
-                resourceView.SetGold(Mathf.RoundToInt(_gold));
+                _gold += GetGoldIncrement();
+                UpdateResourceView();
+                CheckIfCanUpgrade();
             };
+        }
+
+        private void UpdateResourceView()
+        {
+            resourceView.SetGold(Mathf.RoundToInt(_gold));
+        }
+
+        private void CheckIfCanUpgrade()
+        {
+            upgradeView.SetInteractable(_gold >= _nextCost);
+        }
+
+        private float GetGoldIncrement()
+        {
+            return 5 * Mathf.Pow(_level, 2.1F);
+        }
+
+        private int GetUpgradeCost()
+        {
+            return Mathf.RoundToInt(5 * Mathf.Pow(1.08F, _level));
         }
     }
 }
---
+++ Assets/Scripts/UpgradeView.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CvB
+{
+    public delegate void UpgradeViewClickEvent();
+
+    public class UpgradeView : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _upgradeButton;
+
+        [SerializeField]
+        private TMPro.TextMeshProUGUI _label;
+
+        private const string UPGRADE_LABEL = "Upgrade {0}";
+
+        public event UpgradeViewClickEvent OnClick;
+
+        private void Start()
+        {
+            _upgradeButton.onClick.AddListener(() =>
+            {
+                if(OnClick != null)
+                {
+                    OnClick();
+                }
+            });
+        }
+
+        public void ShowUpgradeCost(int ammount)
+        {
+            _label.text = string.Format(UPGRADE_LABEL, ammount);
+        }
+
+        public void SetInteractable(bool isInteractable)
+        {
+            _upgradeButton.interactable = isInteractable;
+        }
+    }
+}
