22d60fa01594713e6266c84abf3d1cf02edc60dd
Dalius Grazinskis <grazinskisd@gmail.com>
2019-10-08 19:18:13 +0200

    Add simple click for enemy and score increment

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CvB
+{
+    public class GameController : MonoBehaviour
+    {
+        public Enemy enemy;
+        public ResourceView resourceView;
+
+        private float _gold = 0;
+        private int _level = 1;
+
+        private void Start()
+        {
+            enemy.OnClicked += () =>
+            {
+                _gold += 5 * Mathf.Pow(_level, 2.1F);
+                resourceView.SetGold(Mathf.RoundToInt(_gold));
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/ResourceView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace CvB
+{
+    public class ResourceView : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _goldLabel;
+
+        private const string LABEL_FORMAT = "Gold: {0}";
+
+        public void SetGold(int ammount)
+        {
+            // TODO: Int might not be enough, might have to format it
+            _goldLabel.text = string.Format(LABEL_FORMAT, ammount);
+        }
+    }
+}
