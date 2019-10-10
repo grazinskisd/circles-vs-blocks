8ff724e9937f4000f7e0f2c484af778bc5548262
Dalius Grazinskis <grazinskisd@gmail.com>
2019-10-10 20:38:41 +0200

    [WIP] Circles

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace CvB
+{
+    public delegate void CircleClickEvent();
+
+    public class Circle : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshPro _label;
+
+        [SerializeField]
+        private Color _enabledColor;
+
+        [SerializeField]
+        private Color _disabledColor;
+
+        public event CircleClickEvent OnClick;
+
+        private bool _interactable;
+
+        private void OnMouseDown()
+        {
+            if(_interactable && OnClick != null)
+            {
+
+            }
+        }
+
+        public bool interactable
+        {
+            set
+            {
+                _interactable = value;
+            }
+
+            get
+            {
+                return _interactable;
+            }
+        }
+
+        public void SetPrice(string price)
+        {
+            _label.text = price;
+        }
+    }
+}
