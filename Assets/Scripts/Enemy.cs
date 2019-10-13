694c8dbdaa77f41e6a9cfd42b59b9fe428e83ac6
Dalius Grazinskis <grazinskisd@gmail.com>
2019-10-13 15:47:48 +0200

    Simple click animation for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 namespace CvB
 {
@@ -6,13 +7,43 @@ namespace CvB
 
     public class Enemy : MonoBehaviour
     {
+        [Header("Tweening")]
+        [SerializeField]
+        private float _scaleDownOnClick;
+
+        [SerializeField]
+        private float _scaleDurationInSeconds;
+
         public event ClickEvent OnClicked;
+
+        private Vector3 _startScale;
+        private Tween _scaleTween;
+
+        private void Start()
+        {
+            _startScale = transform.localScale;
+
+            _scaleTween = transform.DOScale(_scaleDownOnClick, _scaleDurationInSeconds)
+                .SetEase(Ease.InOutQuad)
+                .SetLoops(2, LoopType.Yoyo)
+                .SetAutoKill(false);
+            _scaleTween.Pause();
+        }
 
         private void OnMouseDown()
         {
+            PlayClickAnimation();
             if (this.OnClicked != null)
             {
                 OnClicked();
+            }
+        }
+
+        private void PlayClickAnimation()
+        {
+            if (!_scaleTween.IsPlaying())
+            {
+                _scaleTween.Restart();
             }
         }
     }
